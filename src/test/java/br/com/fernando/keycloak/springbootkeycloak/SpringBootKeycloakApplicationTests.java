ac66258dbed600f3e05deba71459445e304891bb
fernandoguide <fernando-guide@hotmail.com>
2020-08-30 09:54:06 -0300

    autenticacao keycloak

---
+++ src/test/java/br/com/fernando/keycloak/springbootkeycloak/SpringBootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernando.keycloak.springbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
